8604c3857cf28584317f2ac37a1be8c963672a69
victormnoel <victor-siqueira01@hotmail.com>
2025-09-09 03:50:24 -0300

    [Feature] Implementação da camada API do backend.

--- Backend/AgendaTelefonica.API/Program.cs
+++ Backend/AgendaTelefonica.API/Program.cs
@@ -1,3 +1,7 @@
+using AgendaTelefonica.Aplicacao.Commands.Cadastrar;
+using AgendaTelefonica.IOC;
+using MediatR;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AdicionarServicos();
+builder.Services.AddMediatR(typeof(CadastrarUsuarioCommand));
 
 var app = builder.Build();
 
